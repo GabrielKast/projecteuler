317da3ded59a0d9362eb4e8e59a4d9b7220f6d16
gabriel <gabriel@denpasar.(none)>
2010-10-18 00:17:14 +0200

    pb5 brutal force... Slow but ok

--- pb5.erl
+++ pb5.erl
@@ -6,15 +6,18 @@
 %% What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 -module(pb5).
--export([pb5/0]).
+-export([pb5/0, pb5/1]).
 
 pb5 ()->
-    is_ok(20).
-is_ok(N)->
-    io:format("~nDebut de test avec ~p : ", [N]),
-    is_divisible(N, 20) orelse is_ok(N+20).
+    is_ok(20, 20).
+pb5 (Pas) -> is_ok(Pas, Pas). 
+is_ok(N, Pas)->
+    % io:format("~nDebut de test avec ~p : ", [N]),
+    is_divisible(N, Pas) orelse is_ok(N+Pas, Pas).
+
 is_divisible(N, 1) ->
-    N;
+    io:format("Trouve : ~p~n", [N]),
+    true;
 is_divisible(N, X) -> 
-    io:format("~p / ~p ?", [N, X]),
+    % io:format("~p / ~p ?", [N, X]),
     N rem X =:=0 andalso is_divisible(N, X-1).
