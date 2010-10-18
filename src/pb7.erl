8e327fa423f48b85f79a26bf6e408b5fc77d9a4e
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2010-10-18 02:42:01 +0200

    Pb7 : What is the 10001st prime number?

--- src/pb7.erl
+++ src/pb7.erl
@@ -2,3 +2,26 @@
 %% By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 %% What is the 10001st prime number?
+-module(pb7).
+-import(pb3, [is_prime/1]).
+-export([pb7/0, pb7/1]).
+
+pb7()->
+    pb7(10001).
+pb7(N)->
+    L=list_of_prime(2, N, 0,  []), 
+    lists:last(L).
+list_of_prime(X, N, Size_List, List)->
+    Next_Prime= X+1 + (X rem 2),
+    case { is_prime (X), Size_List+1 } of
+	    {true,  N } ->
+		    lists:reverse([ X | List ])  ; %% End of the search
+	    {true, _ } ->
+		    list_of_prime (Next_Prime, N, Size_List+1, [ X|List ]);
+	    {false, _ } ->
+		    list_of_prime (Next_Prime, N, Size_List, List)
+    end.
+
+
+			
+
--- test/pbs_tests.erl
+++ test/pbs_tests.erl
@@ -1,9 +1,12 @@
 % Required 
-% code:add_path("/usr/lib/erlang/lib/eunit-2.1.4/ebin").
+% code:add_path("/usr/lib/erlang/lib/eunit-2.1.4/ebin")
+
 
 -module(pbs_tests).
 -include_lib("eunit/include/eunit.hrl").
-
 pb5_test() ->
     ?assertEqual(2520, pb5:pb5(10)).
 
+%% Check that the 6st prime is 13
+pb7_test() ->
+    ?assertEqual(13, pb7:pb7(6)).
