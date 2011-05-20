19438cd2c45e51f7f10acd36554e9e29c3932496
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2011-05-20 08:16:23 +0200

    create directory per language

---
+++ erlang/src/pb1.erl
@@ -0,0 +1,16 @@
+%% For : http://projecteuler.net
+%% 
+%% Add all the natural numbers below one thousand that are multiples of 3 or 5.
+%% If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+%% Find the sum of all the multiples of 3 or 5 below 1000.
+-module(pb1).
+-export([pb1/1, pb1/0]).
+-import(lists, [seq/3, foldl/3]).
+
+pb1()->
+    pb1(10000).
+
+pb1(N)->
+    S=lists:sum([X || X <- seq(1,N-1, 1), X rem 3=:=0 orelse X rem 5 =:=0 ]),
+    io:fwrite("pb1:pb1/1:S=~p~n",[S]),
+    S.
